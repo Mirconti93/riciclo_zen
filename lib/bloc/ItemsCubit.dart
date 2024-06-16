f0f3360d53411af4da5c4d5e667802077f333e04
mirconti <mirconti93@gmail.com>
2024-06-16 14:47:51 +0200

    add domain

--- lib/bloc/ItemsCubit.dart
+++ lib/bloc/ItemsCubit.dart
@@ -7,6 +7,8 @@ import 'dart:developer';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:riciclo_zen/data/datasource/ItemsDataSourceImpl.dart';
+import 'package:riciclo_zen/data/repository/ItemsRepositoryImpl.dart';
 import '../domain/models/ItemModel.dart';
 import 'ItemsState.dart';
 
@@ -16,23 +18,26 @@ class ItemsCubit extends Cubit<ItemsState> {
   ItemsCubit() : super(ItemsState(const []));
 
   void fetchData() async {
-    await Firebase.initializeApp();
-    DatabaseReference _databaseReference = FirebaseDatabase.instance.ref('Oggetti');
-    _databaseReference.onValue.listen((DatabaseEvent event) {
-      Map<dynamic, dynamic> values = event.snapshot.value  as Map<dynamic, dynamic>;
-      if (values.isEmpty) {
-        notifyError("Nessun elemento trovato");
-      } else {
-        log('db riciclo lenght: ${values.length}');
-        List<ItemModel> items = [];
-        values.forEach((key, value) {
-          log('db value: $value');
-          items.add(ItemModel.fromJson(key, value));
-        });
-        itemList = items;
-        emit(ItemsState(items));
-      }
-    });
+    var data=ItemsDataSourceImpl().fetchData();
+    emit(ItemsState(data));
+
+    // await Firebase.initializeApp();
+    // DatabaseReference _databaseReference = FirebaseDatabase.instance.ref('Oggetti');
+    // _databaseReference.onValue.listen((DatabaseEvent event) {
+    //   Map<dynamic, dynamic> values = event.snapshot.value  as Map<dynamic, dynamic>;
+    //   if (values.isEmpty) {
+    //     notifyError("Nessun elemento trovato");
+    //   } else {
+    //     log('db riciclo lenght: ${values.length}');
+    //     List<ItemModel> items = [];
+    //     values.forEach((key, value) {
+    //       log('db value: $value');
+    //       items.add(ItemModel.fromJson(key, value));
+    //     });
+    //     itemList = items;
+    //     emit(ItemsState(items));
+    //   }
+    // });
 
   }
 
--- lib/data/datasource/ItemsDataSourceImpl.dart
+++ lib/data/datasource/ItemsDataSourceImpl.dart
@@ -3,7 +3,7 @@ import 'dart:developer';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 
-import '../domain/models/ItemModel.dart';
+import '../../domain/models/ItemModel.dart';
 
 class ItemsDataSourceImpl {
 
